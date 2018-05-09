3c8537c6accafb1c1f7777e6834d03184def3efd
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2018-05-09 21:36:18 +0100

    Added RPM Fusion and Steam to the install

--- fedora-bootstrap.sh
+++ fedora-bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
 sudo dnf install -y git vim xclip libselinux-python
 sudo pip install ansible boto boto3
