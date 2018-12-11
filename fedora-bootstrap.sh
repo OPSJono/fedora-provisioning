207daef353e80c693cfc0a7dcbaa81e4866556df
Jonathan Marshall <jonathan@localhost.localdomain>
2018-12-11 11:04:12 -0500

    Added SCSS to the pip install

--- fedora-bootstrap.sh
+++ fedora-bootstrap.sh
@@ -2,6 +2,6 @@
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
 sudo dnf install -y git vim xclip libselinux-python
-sudo pip3 install ansible boto boto3
+sudo pip3 install ansible boto boto3 scss
 sudo mkdir /etc/ansible/
 sudo cp ansible_hosts /etc/ansible/hosts
