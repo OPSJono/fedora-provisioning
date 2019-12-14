087c0dd88216cb9556d4c74b82978536b9ac1628
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2019-12-14 21:43:30 +0000

    Added mysql server

--- fedora-bootstrap.sh
+++ fedora-bootstrap.sh
@@ -2,7 +2,7 @@
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
 sudo dnf install -y git vim-enhanced xclip libselinux-python
-sudo pip3 install ansible boto boto3 scss
+sudo pip3 install ansible boto boto3 scss PyMySQL
 sudo pip install boto boto3
 sudo mkdir /etc/ansible/
 sudo cp ansible_hosts /etc/ansible/hosts
