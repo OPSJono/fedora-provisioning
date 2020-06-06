5858c60ccb04accae0b1913931ebca4efa7b54bf
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2020-06-06 23:11:39 +0100

    Fix for Docker installation on F32.
    Moved the VPN packages into an ansible role.

--- fedora-bootstrap.sh
+++ fedora-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
-sudo dnf install -y git vim vim-enhanced xclip strongswan
+sudo dnf install -y git vim vim-enhanced xclip
 sudo pip3 install ansible boto boto3 scss PyMySQL
 sudo pip install boto boto3
 sudo mkdir /etc/ansible/
